feaca395982aba24d8f10915bf27e2d7c476e62f
melodia <melodiayaoivv@gmail.com>
2025-12-17 23:17:15 -0300

    upd

--- index.js
+++ index.js
@@ -33,12 +33,12 @@ const PORT = process.env.PORT || process.env.SERVER_PORT || 3000
 
 let { say } = cfonts
 console.log(chalk.magentaBright('\n❀ Iniciando...'))
-say('Yuki Suou', {
+say('OguriCap-Bot', {
 font: 'simple',
 align: 'left',
 gradient: ['green', 'white']
 })
-say('Made with love by Destroy', {
+say('Made with love from Melodia', {
 font: 'console',
 align: 'center',
 colors: ['cyan', 'magenta', 'yellow']
