124d763b30ae10dcad0b6f9e7353ed0ca008039c
melodia <melodiayaoivv@gmail.com>
2025-12-27 18:33:24 -0300

    upd

--- index.js
+++ index.js
@@ -244,9 +244,10 @@ phoneNumber = `+${phoneNumber}`
 rl.close()
 addNumber = phoneNumber.replace(/\D/g, '')
 setTimeout(async () => {
-let codeBot = await conn.requestPairingCode(addNumber, null)
-codeBot = codeBot.match(/.{1,4}/g)?.join("-") || codeBot
-console.log(chalk.bold.white(chalk.bgMagenta(`[ ✿ ]  Código:`)), chalk.bold.white(chalk.white(codeBot)))
+  // ✅ CAMBIO ÚNICO: IGUAL A YUKI (sin pairKey)
+  let codeBot = await conn.requestPairingCode(addNumber)
+  codeBot = codeBot.match(/.{1,4}/g)?.join("-") || codeBot
+  console.log(chalk.bold.white(chalk.bgMagenta(`[ ✿ ]  Código:`)), chalk.bold.white(chalk.white(codeBot)))
 }, 3000)
 }}}}
 conn.isInit = false
